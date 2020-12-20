d13571be5de9d82bd3e36eb5114719ec7fbc0233
lizandro94 <leandrodfa94@gmail.com>
2020-12-19 22:01:21 -0600

    Add File editor proyect

---
+++ Tools/FileEditor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("C:\\Code\\LasFlores\\members.json");
+            dynamic membersList = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+            foreach (var member in membersList)
+            {
+                member["householdMember"] = string.Empty;
+                member["address"] = string.Empty;
+                member["personStatusFlags"] = string.Empty;
+                member["birth"] = string.Empty;
+            }
+
+            //jsonObj["Bots"][0]["Password"] = "new password";
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(membersList, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText("C:\\Code\\LasFlores\\members.json", output);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
