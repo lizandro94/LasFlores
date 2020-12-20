00482d9d2e8b4c9aeab73ccf2eb68fa789a13066
lizandro94 <leandrodfa94@gmail.com>
2020-12-19 23:12:31 -0600

    Add ant design

--- LasFlores.Spa/src/components/fasting/DocumentsGenerator.js
+++ LasFlores.Spa/src/components/fasting/DocumentsGenerator.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
 
 const DocumentsGenerator = () => {
-    return (
-        <div>
-            test
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>antd version: {version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsGenerator;
